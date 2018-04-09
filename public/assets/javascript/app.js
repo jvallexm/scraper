c7f716c1843c2d5fa2dee86e48d6c03467776bd0
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-04-09 17:26:48 -0400

    Minor fixes

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -2,10 +2,18 @@ $(document).ready(()=>{
     $(".show").on("click",function(){
         let id = $(this).attr("data-id");
         console.log(id);
-        if( $("#comments-" + id).hasClass("hidden") )
+
+        if( $("#comments-" + id).hasClass("hidden") ){
+
             $("#comments-" + id).removeClass("hidden");
-        else
+            $("#show-" + id).text("Hide");
+
+        }else{
+
             $("#comments-" + id).addClass("hidden");
+            $("#show-" + id).text("Show");
+
+        }
     });
     $(".panel-heading").on("click",function(){
         window.open($(this).attr("data-link"));
